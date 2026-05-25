edf4421d1326e1ddf6fa6ec1cc7e78a43f03d2ac
Sasithorn Khamsong <sasithorn.khamsong@gmail.com>
2026-05-25 23:41:55 +0700

    5/25

--- Assets/Script/Progress/ProgressData.cs
+++ Assets/Script/Progress/ProgressData.cs
@@ -48,12 +48,38 @@ public class ProgressData : MonoBehaviour
         }
 
         // ---------- Recent 5 Days ----------
+        string today =
+            System.DateTime.Now.ToString("yyyyMMdd");
+
+        string lastRecentDate =
+            PlayerPrefs.GetString(
+                gameName + "_LastRecentDate",
+                ""
+            );
+
+        // เพิ่ม recent แค่ครั้งแรกของวัน
+        if (lastRecentDate != today)
+        {
+            ShiftRecent(gameName);
+
+            if (isPersonalBest)
+                PlayerPrefs.SetInt(gameName + "_Recent_0", 2);
+            else
+                PlayerPrefs.SetInt(gameName + "_Recent_0", 1);
+
+            PlayerPrefs.SetString(
+                gameName + "_LastRecentDate",
+                today
+            );
+        }
+
+        /*// ---------- Recent 5 Days ----------
         ShiftRecent(gameName);
 
         if (isPersonalBest)
             PlayerPrefs.SetInt(gameName + "_Recent_0", 2);
         else
-            PlayerPrefs.SetInt(gameName + "_Recent_0", 1);
+            PlayerPrefs.SetInt(gameName + "_Recent_0", 1);*/
 
         // ---------- STREAK ----------
         string today =
@@ -148,15 +174,46 @@ public class ProgressData : MonoBehaviour
 
     // ======= คำนวณ % ของแต่ละเกม =======
     // maxScore และ totalQuests ต้องกำหนดตามแต่ละเกม
+    /*public float GetProgress(string gameName)
+    {
+        float progress = 0f;
+
+        // ===== ความสม่ำเสมอ 20% ===== ต้องแก้เพิ่ม ===== 
+        int playCount =
+            PlayerPrefs.GetInt(gameName + "_PlayCount", 0);
+
+        progress += Mathf.Clamp01(playCount / 5f) * 0.2f; // ==== เล่นมากกว่า 5 ครั้งได้ =====
+        // ==== เพิ่มจังหวะก่อน reset =====  เผื่อผู้เล่นไม่เห็น success ของตัวเอง
+
+        // ===== คะแนนพัฒนาการ 40% =====
+        int bestScore =
+            GetBestScore(gameName);
+
+        progress += Mathf.Clamp01(bestScore / 500f) * 0.4f;
+
+        // ===== ความครบถ้วน 40% =====
+        int completedQuest = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (IsQuestComplete(gameName, i))
+                completedQuest++;
+        }
+
+        progress += (completedQuest / 3f) * 0.4f;
+
+        return Mathf.Clamp01(progress);
+    }*/
+
     public float GetProgress(string gameName)
     {
         float progress = 0f;
 
         // ===== ความสม่ำเสมอ 20% =====
-        int playCount =
-            PlayerPrefs.GetInt(gameName + "_PlayCount", 0);
+        int playDays =
+            PlayerPrefs.GetInt(gameName + "_PlayDays", 0);
 
-        progress += Mathf.Clamp01(playCount / 5f) * 0.2f;
+        progress += Mathf.Clamp01((playDays - 1) / 5f) * 0.2f;
 
         // ===== คะแนนพัฒนาการ 40% =====
         int bestScore =
